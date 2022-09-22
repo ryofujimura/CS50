e94ec014caa1a4e06d1e6accf6690ef481f39db1
Ryo Fuji <ryo176999@gmail.com>
2022-09-22 09:27:45 -0700

    mybirthday

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hello',
+    'mybirthday',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Django/mybirthday/views.py
+++ Django/mybirthday/views.py
@@ -1,3 +1,11 @@
+import datetime
 from django.shortcuts import render
 
+# from Django import mybirthday
+
 # Create your views here.
+def index(request):
+    now = datetime.datetime.now()
+    return render(request, "mybirthday/index.html", {
+        "mybirthday": now.month == 7 and now.day == 6
+    })
