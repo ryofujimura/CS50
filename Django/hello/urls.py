8685afb53d1f63db3f5904c40387440bb3758aa1
Ryo Fuji <ryo176999@gmail.com>
2022-09-21 23:43:32 -0700

    responsive

--- Django/hello/urls.py
+++ Django/hello/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("me", views.me, name="me"),
+    path("<str:name>", views.greet, name="greet")
+
 ]
--- Django/hello/views.py
+++ Django/hello/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render(request, "hello/index.html")
+
+def me(request):
+    return HttpResponse("Hello, my name is <your name>!")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    }
+    )
